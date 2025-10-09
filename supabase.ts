d63d01f89aa6529b5b2225a8ff3f35ea849837b1
hmainspiration <hmalldm95@gmail.com>
2025-10-09 17:53:08 -0600

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,12 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Estas variables se cargan automáticamente desde .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-// Verificar que las variables estén cargadas
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.error('Error: Faltan las variables de entorno de Supabase')
+  console.error('Error: Faltan las variables de entorno de Supabase');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
